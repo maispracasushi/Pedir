2458ad7cdcaa3a3013e5be6ad38d9802b902dac8
maispracasushi <maispracasushi@gmail.com>
2026-02-26 01:31:17 -0300

    Add service worker for caching assets

---
+++ service-worker.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = "maispracadelivery-v1";
+
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/logo.png",
+  "/icon-192.png",
+  "/icon-512.png",
+  "/manifest.json"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});

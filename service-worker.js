ccdde6eda03d0edb2b0c0c02c8fe31a5f5acbd39
maispracasushi <maispracasushi@gmail.com>
2026-02-26 02:08:44 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,24 +1,42 @@
-const CACHE_NAME = "maispracadelivery-v1";
+const CACHE_NAME = "maispracasusshi-v1";
 
 const urlsToCache = [
-  "/",
-  "/index.html",
-  "/logo.png",
-  "/icon-192.png",
-  "/icon-512.png",
-  "/manifest.json"
+  "/Pedir/",
+  "/Pedir/index.html",
+  "/Pedir/manifest.json",
+  "/Pedir/icon-192.png",
+  "/Pedir/icon-512.png"
 ];
 
+// instala
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
   );
+  self.skipWaiting();
 });
 
+// ativa
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(names => {
+      return Promise.all(
+        names.map(name => {
+          if (name !== CACHE_NAME) return caches.delete(name);
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// fetch
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
